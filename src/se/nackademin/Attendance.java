47c83a0ac02313fa4e0074d3e135979d27dae6c4
Albin Gudmundsson <albin.gudmundsson@gmail.com>
2019-10-13 14:38:32 +0200

    adding attendance now writes to file

--- src/se/nackademin/Attendance.java
+++ src/se/nackademin/Attendance.java
@@ -1,5 +1,7 @@
 package se.nackademin;
+import java.io.*;
 import java.time.*;
+import java.util.*;
 
 public class Attendance {
     private String personnummer;
@@ -10,6 +12,12 @@ public class Attendance {
         this.personnummer = personnummer;
         this.name = name;
         this.date = date;
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("attendance.txt", true))) {
+            bw.write(personnummer +"\n" + name + "\n" + date.toString() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } ;
     }
 
     @Override
